f83a0f02acca69d15c7c44302a1f9caaa8b3c8e9
Max Thayer <garbados@gmail.com>
2014-01-30 19:56:11 -0500

    no longer ignored test dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     files: [
       'dist/js/vendor.js',
       'dist/js/bundle.js',
-      'tests/lib/*.js',
+      'tests/reqs/*.js',
       'tests/unit/*.js',
       'tests/midway/*.js',
       'tests/e2e/*.js'
@@ -37,7 +37,7 @@ module.exports = function(config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'dist/js/*.js': ['coverage']
+      'dist/js/bundle.js': ['coverage']
     },
 
     coverageReporter: {
